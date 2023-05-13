e1b97af97d9d8ec09c653c91655d27076821ed99
ericlisribeiro <125934839+ericlisribeiro@users.noreply.github.com>
2023-05-12 23:09:08 -0300

    att

---
+++ aula12/ex011.js
@@ -0,0 +1,9 @@
+var fortuna = 220
+console.log(`parabéns sua fortuna é de ${fortuna} reais`)
+if (fortuna < 100) {
+    console.log('Voçê é rico')
+} else if (fortuna = 100) {
+    console.log('Voçê é milionario')
+} else {
+    console.log('voçê é bilionário')
+}
