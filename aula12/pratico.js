ff86dc463c080facd1307bae486c64b4b9c61f7a
ericlisribeiro <125934839+ericlisribeiro@users.noreply.github.com>
2023-05-17 00:45:36 -0300

    att

---
+++ aula12/pratico.js
@@ -0,0 +1,16 @@
+var momento = new Date()
+var diadasem = momento.getDay()
+
+switch(diadasem) {
+    case 0: 
+        console.log('Domingo')
+        break
+    case 1: 
+        console.log('segunda')
+        break
+    case 2: 
+        console.log('terça')
+        break
+
+    
+}
