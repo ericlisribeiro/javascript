5640634988a916d4cf847eb05a0230c8ec3f7389
ericlisribeiro <125934839+ericlisribeiro@users.noreply.github.com>
2023-05-19 00:15:34 -0300

    att

--- praticos/ex015/script.js
+++ praticos/ex015/script.js
@@ -6,8 +6,47 @@ function verificar() {
    if (fano.value.length == 0 || Number(fano.value) > ano) {
     window.alert('[ERRO] Verifique os dados e tente novamente!')
    } else {
-        var fsex = document.getElementsByName('sex')[0]
+        var fsex = document.getElementsByName('sex')
         var idade = ano - Number(fano.value)
-        res.innerHTML = `idade calculada: ${idade}`
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            gênero = 'homem'
+            if (idade >=0 && idade < 10 ) {
+               //criança
+               img.setAttribute('src', 'imagens/bebêmasc.png')
+            }else if (idade < 21) {
+               // jovem
+               img.setAttribute('src', 'imagens/jovemmasc.png')
+            }else if (idade < 50) {
+               // adulto
+               img.setAttribute('src', 'imagens/adultomasc.png')
+            }else {
+               // idoso
+               img.setAttribute('src', 'imagens/idosomasc.png')
+            }
+
+        }else if (fsex[1].checked) {
+            gênero = 'mulher'
+            if (idade >=0 && idade < 10) {
+               // criança
+               img.setAttribute('src', 'imagens/bebêfem.png')
+            }else if (idade < 21) {
+               // jovem
+               img.setAttribute('src', 'imagens/jovemfem.png')
+            }else if (idade < 50) {
+               // adulto
+               img.setAttribute('src', 'imagens/adultafem.png')
+            } else {
+               // idoso
+               img.setAttribute('src', 'imagens/idosofem.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        img.style.display = 'block'
+        img.style.margin = 'auto'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
    }
 }
